d0628f91289650ec37b73c965c984b95bbab8e98
xiakai <xiakai@51cto.com>
2018-08-24 09:45:46 +0800

    fix main consumer autoload file path

--- MainConsumer.php
+++ MainConsumer.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once __DIR__ . '/../../autoload.php';
 
 use Cto\Rabbit\Consumer\AbstractConsumer;
 use PhpAMqplib\Message\AMQPMessage;
