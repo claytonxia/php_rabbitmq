e77aa6495b9871fc23be6f827829b34654b03e46
xiakai <xiakai@51cto.com>
2018-10-30 10:43:53 +0800

    demo publisher and consumer

--- DemoConsumer.php
+++ DemoConsumer.php
@@ -5,11 +5,11 @@ require_once __DIR__ . '/../../autoload.php';
 use Cto\Rabbit\Consumer\AbstractConsumer;
 use PhpAMqplib\Message\AMQPMessage;
 
-class MainConsumer extends AbstractConsumer
+class DemoConsumer extends AbstractConsumer
 {
     public function consume(AMQPMessage $message)
     {
         echo $message->body, PHP_EOL;
-        return null;
+        return true;
     }
 }
